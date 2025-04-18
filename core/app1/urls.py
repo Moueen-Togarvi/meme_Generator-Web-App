508ead2dc336af877346239cd3433c7dcc8129d6
Moueen Togarvi <moueenuddintogarvi41@gmail.com>
2025-04-18 21:47:45 +0500

    feat: Add ads.txt for Google Ads integration and update URL routing

--- core/app1/urls.py
+++ core/app1/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
 from . import views
+from django.urls import re_path
+from django.views.static import serve
+from django.conf import settings
+import os
 
 urlpatterns = [
+    re_path(r'^ads\.txt$', serve, {
+        'path': 'ads.txt',
+        'document_root': os.path.join(settings.BASE_DIR, 'core/static'),
+    }),
     path('', views.meme_editor, name='meme_editor'),
     path('fetch-memes/', views.fetch_trending_memes, name='fetch_memes'),
     path('save-meme/', views.save_meme, name='save_meme'),
