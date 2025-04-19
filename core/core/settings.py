836fe80b2365a0fb2f865788c3e096e58de4296d
Moueen Togarvi <moueenuddintogarvi41@gmail.com>
2025-04-19 23:34:02 +0500

    fix: Correct DEBUG setting to False for production safety

--- core/core/settings.py
+++ core/core/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s@-aw3u7zp0kcu847dtk3)yuz&v7@#-79-c9&5zoc@6%bydjl0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', '.now.sh', 'localhost', 'memegen-chi.vercel.app']
 
