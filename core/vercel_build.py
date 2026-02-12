ad3406ae0edf8def803ca79356704fa4984518d5
Moueen-Togarvi <moueenuddintogarvi41@gmail.com>
2026-02-13 01:15:42 +0500

    update

--- core/vercel_build.py
+++ core/vercel_build.py
@@ -1,10 +1,9 @@
 # vercel_build.py
 import os
 from django.core.wsgi import get_wsgi_application
-from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
-app = get_asgi_application()
+app = get_wsgi_application()
 
 
 
